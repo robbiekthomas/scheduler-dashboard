efa403fb1e81a1da7f3a60ccc8306b563bdb6c1b
Robbie Thomas <robbiekt@gmail.com>
2023-03-01 13:08:16 -0800

    add panels componenet to dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,6 +2,41 @@ import React, { Component } from "react";
 
 import classnames from "classnames";
 import Loading from "./Loading";
+import Panel from "./Panel";
+
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6,
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm",
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday",
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3",
+  },
+];
+
+const panels = data.map((panel) => {
+  return (
+    <Panel
+      key={panel.id}
+      id={panel.id}
+      label={panel.label}
+      value={panel.value}
+    />
+  );
+});
 
 class Dashboard extends Component {
   state = { loading: false };
@@ -11,8 +46,9 @@ class Dashboard extends Component {
     if (this.state.loading) {
       return <Loading />;
     }
-
-    return <main className={dashboardClasses} />;
+   
+    return <main className={dashboardClasses}>{panels}</main>
+    
   }
 }
 
